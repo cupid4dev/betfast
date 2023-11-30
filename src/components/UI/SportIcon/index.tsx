562430ff5e9fdaf5b6537eb178709d6ede94bf69
david <davidlong.business526@gmail.com>
2023-11-29 19:22:57 -0800

    Sport Icon updated!

--- src/components/UI/SportIcon/index.tsx
+++ src/components/UI/SportIcon/index.tsx
@@ -1,25 +1,28 @@
 "use client";
 import { IoAmericanFootballOutline } from "react-icons/io5";
-import { GiTennisRacket } from "react-icons/gi";
+import { BiTennisBall } from "react-icons/bi";
 import { TbBallFootball } from "react-icons/tb";
-import { GiHockey } from "react-icons/gi";
+import { LiaHockeyPuckSolid } from "react-icons/lia";
 import { CiBasketball } from "react-icons/ci";
-import { TbCricket } from "react-icons/tb";
+import { TbBallBaseball } from "react-icons/tb";
 import { GiCrosshair } from "react-icons/gi";
+import { BiCricketBall } from "react-icons/bi";
 
 export default function SportIcon({ sportName }: { sportName: string }) {
   return sportName == "TENNIS" ? (
-    <GiTennisRacket className="h-5 w-5" />
+    <BiTennisBall className="h-5 w-5" />
   ) : sportName == "AMERICAN_FOOTBALL" ? (
     <IoAmericanFootballOutline className="h-5 w-5" />
   ) : sportName == "ICEHOCKEY" ? (
-    <GiHockey className="h-5 w-5" />
+    <LiaHockeyPuckSolid className="h-5 w-5" />
   ) : sportName == "BASKETBALL" ? (
     <CiBasketball className="h-5 w-5" />
-  ) : sportName == "T20" ? (
-    <TbCricket className="h-5 w-5" />
+  ) : sportName == "BASEBALL" ? (
+    <TbBallBaseball className="h-5 w-5" />
   ) : sportName == "CSGO" ? (
     <GiCrosshair className="h-5 w-5" />
+  ) : sportName == "CRICKET" || sportName == "T20" ? (
+    <BiCricketBall className="h-5 w-5" />
   ) : (
     <TbBallFootball className="h-5 w-5" />
   );
