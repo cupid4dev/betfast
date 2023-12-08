02cf3138194533cd691248092469a9d274bb8742
david <davidlong.business526@gmail.com>
2023-12-08 06:16:36 -0800

    Icon Changed

--- src/components/UI/SportIcon/index.tsx
+++ src/components/UI/SportIcon/index.tsx
@@ -7,6 +7,12 @@ import { CiBasketball } from "react-icons/ci";
 import { TbBallBaseball } from "react-icons/tb";
 import { GiCrosshair } from "react-icons/gi";
 import { BiCricketBall } from "react-icons/bi";
+import { MdSportsEsports } from "react-icons/md";
+import { MdSportsMma } from "react-icons/md";
+import { GiBoxingGlove } from "react-icons/gi";
+import { MdSportsGolf } from "react-icons/md";
+import { MdOutlineSportsRugby } from "react-icons/md";
+import { RiBilliardsFill } from "react-icons/ri";
 
 export default function SportIcon({ sportName }: { sportName: string }) {
   return sportName == "TENNIS" ? (
@@ -15,7 +21,7 @@ export default function SportIcon({ sportName }: { sportName: string }) {
     <IoAmericanFootballOutline className="h-5 w-5" />
   ) : sportName == "ICEHOCKEY" ? (
     <LiaHockeyPuckSolid className="h-5 w-5" />
-  ) : sportName == "BASKETBALL" ? (
+  ) : sportName == "BASKETBALL" || sportName == "CBB" ? (
     <CiBasketball className="h-5 w-5" />
   ) : sportName == "BASEBALL" ? (
     <TbBallBaseball className="h-5 w-5" />
@@ -23,6 +29,18 @@ export default function SportIcon({ sportName }: { sportName: string }) {
     <GiCrosshair className="h-5 w-5" />
   ) : sportName == "CRICKET" || sportName == "T20" ? (
     <BiCricketBall className="h-5 w-5" />
+  ) : sportName == "ESPORTS" || sportName == "DOTA" || sportName == "LOL" ? (
+    <MdSportsEsports className="h-5 w-5" />
+  ) : sportName == "MMA" ? (
+    <MdSportsMma className="h-5 w-5" />
+  ) : sportName == "BOXING" ? (
+    <GiBoxingGlove className="h-5 w-5" />
+  ) : sportName == "GOLF" ? (
+    <MdSportsGolf className="h-5 w-5" />
+  ) : sportName == "RUGBY" ? (
+    <MdOutlineSportsRugby className="h-5 w-5" />
+  ) : sportName == "SNOOKER" ? (
+    <RiBilliardsFill className="h-5 w-5" />
   ) : (
     <TbBallFootball className="h-5 w-5" />
   );
