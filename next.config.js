fd0b717e4ec37c4f4a498b19f58bedee81f4099a
david <davidlong.business526@gmail.com>
2023-12-21 22:35:22 -0800

    Proxy Set!

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/serverapi/:path',
+        destination: 'http://173.231.222.233:5000/api/:path',
+      },
+    ]
+  },
+}
--- src/constants/mpRouter.tsx
+++ src/constants/mpRouter.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
   // API_URL: "http://localhost:5000/api",
-  API_URL: "http://173.231.222.233:5000/api",
+  API_URL: "/serverapi",
 };
